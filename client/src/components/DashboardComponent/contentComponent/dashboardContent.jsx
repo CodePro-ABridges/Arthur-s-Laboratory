8f710873e00c5c8be573d1299796446d7d99ad00
CodePro-ABridges <arthurbridges08@yahoo.com>
2024-06-25 14:06:17 -0500

    CHANGED: Hovering Post
    When hovering post, the border color changes and added cursor pointer.

--- client/src/components/DashboardComponent/contentComponent/dashboardContent.jsx
+++ client/src/components/DashboardComponent/contentComponent/dashboardContent.jsx
@@ -12,14 +12,14 @@ const MainContent = () => {
   return (
     <div className="px-6 text-neutral-400">
       {posts.length === 0 ? (
-        <div className="border border-neutral-600 bg-neutral-900 mt-4 p-2 rounded-md cursor-pointer">
+        <div className="border border-neutral-600 bg-neutral-900 mt-4 p-2 rounded-md">
           <div className="text-center text-neutral-600 text-sm">No Posts</div>
         </div>
       ) : (
         posts.map((post) => (
           <div
             key={post._id}
-            className="border border-neutral-600 bg-neutral-900 mt-4 p-4 rounded-md mb-4"
+            className="border border-neutral-600 hover:border-yellow-200 bg-neutral-900 mt-4 p-4 rounded-md mb-4 cursor-pointer"
           >
             <h5 className="text-neutral-600 text-sm mb-2">
               Posted by user/{post?.author?.name} on{" "}
