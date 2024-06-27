b0cb55a4d76669d40b1e39778e535c79e27a7de9
CodePro-ABridges <arthurbridges08@yahoo.com>
2024-06-27 10:05:02 -0500

    CHANNGES: PostFormModal
    Added the button 'Cancel'

--- client/src/components/ModalComponents/PostFormModalComponent/PostFormModal.jsx
+++ client/src/components/ModalComponents/PostFormModalComponent/PostFormModal.jsx
@@ -44,7 +44,13 @@ const PostFormModal = ({ onClose }) => {
             />
             <div className="text-center">
               <button
-                className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700"
+                className="mt-4 px-4 py-2 mr-3 bg-red-500 text-white rounded hover:bg-red-700"
+                onClick={onClose}
+              >
+                Cancel
+              </button>
+              <button
+                className="mt-4 px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-700"
                 type="submit"
               >
                 Post
