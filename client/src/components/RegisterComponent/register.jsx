2629133c26a7933d111019de701616a98f17bfa8
CodePro-ABridges <arthurbridges08@yahoo.com>
2024-07-20 21:41:30 -0500

    REFACTOR: Login and Register Components
    Refactored for clairty and readability

--- client/src/components/RegisterComponent/register.jsx
+++ client/src/components/RegisterComponent/register.jsx
@@ -5,60 +5,80 @@ import AuthModal from "../ModalComponents/authModal/authModal.jsx";
 
 const RegisterForm = () => {
   //States
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirmation, setPasswordConfirmation] = useState("");
-  const [error, setError] = useState("");
+  const [registerForm, setRegisterForm] = useState({
+    email: "",
+    name: "",
+    password: "",
+    passwordConfirmation: "",
+    errorState: "",
+  });
+
   const { register } = useAuth();
 
+  //Navigation.
   const navigate = useNavigate();
+
+  //Handle Change
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRegisterForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  //Set error state
+  const setErrorState = (message) => {
+    setRegisterForm((prev) => ({ ...prev, errorState: message }));
+  };
 
   //submit the form
   const handleSubmit = async (e) => {
-    //Wipe the form
     e.preventDefault();
+
+    const { email, name, password, passwordConfirmation } = registerForm;
+
     //Clear previous errors.
-    setError("");
-    //Conditional for not entering an email.
+    setErrorState("");
+
+    //Validations
     if (!email) {
-      setError("Email is required");
+      setErrorState("Email is required");
       return;
     }
-    //Conditional for not entering username
     if (!name) {
-      setError("Name is required");
+      setErrorState("Name is required");
       return;
     }
-    //conditional for password === passwordConfirmation
     if (password !== passwordConfirmation) {
-      setError("Passwords do not match");
+      setErrorState("Passwords do not match");
       return;
     }
-    //nested try that when registered it'll log them in after register.
+
     try {
       const { success, error } = await register(name, email, password);
-      //conditional for registering the user then sending them to dashboard.
       if (success) {
-        //TODO: Change to dashboard when Dashboard component is finished.
         navigate("/dashboard");
       } else {
-        setError(error);
+        setErrorState(error);
       }
     } catch (err) {
-      setError("Registration failed: ", err);
+      setErrorState(`Registration failed: ${err.message}`);
     }
   };
 
-  //render the form
   return (
     <div className="w-screen h-screen flex top-0 justify-center items-center">
-      {error && <AuthModal message={error} onClose={() => setError("")} />}
+      {registerForm.errorState && (
+        <AuthModal
+          message={registerForm.errorState}
+          onClose={() => setErrorState("")}
+        />
+      )}
       <div className="bg-white p-6 rounded-lg shadow-lg border border-neutral-700 w-3/4 sm:w-1/2 md:w-1/3">
         <form onSubmit={handleSubmit}>
           <h1 className="text-2xl mb-5 text-center">Register</h1>
-          {/**/}
-          {/*Email div*/}
+
           <div className="text-center mb-4">
             <label className="block mb-1">Email</label>
             <input
@@ -66,11 +86,11 @@ const RegisterForm = () => {
               type="email"
               name="email"
               placeholder="Enter the email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              value={registerForm.email}
+              onChange={handleChange}
             />
           </div>
-          {/*name div*/}
+
           <div className="text-center mb-4">
             <label className="block mb-1">Username</label>
             <input
@@ -78,11 +98,11 @@ const RegisterForm = () => {
               type="text"
               name="name"
               placeholder="Enter the username"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              value={registerForm.name}
+              onChange={handleChange}
             />
           </div>
-          {/*Password div*/}
+
           <div className="text-center mb-4">
             <label className="block mb-1">Password</label>
             <input
@@ -90,11 +110,11 @@ const RegisterForm = () => {
               type="password"
               name="password"
               placeholder="Enter the password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              value={registerForm.password}
+              onChange={handleChange}
             />
           </div>
-          {/*Password confirmation div*/}
+
           <div className="text-center mb-4">
             <label className="block mb-1">Confirm Password</label>
             <input
@@ -102,11 +122,11 @@ const RegisterForm = () => {
               type="password"
               name="passwordConfirmation"
               placeholder="Confirm password"
-              value={passwordConfirmation}
-              onChange={(e) => setPasswordConfirmation(e.target.value)}
+              value={registerForm.passwordConfirmation}
+              onChange={handleChange}
             />
           </div>
-          {/*Button submit div*/}
+
           <div className="text-center">
             <button
               className="bg-yellow-400 text-white px-4 py-2 rounded hover:bg-yellow-500 w-full"
